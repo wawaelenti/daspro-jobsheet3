314adbc5230f4078043b5f9fb5a5e777e5dbae21
wawaelenti <irawantielent@gmail.com>
2024-09-12 14:03:21 +0700

    add daspro 3 case study

---
+++ Kafe25.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Kafe25 { 
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        boolean keanggotaan;
+        int jmlKopi, jmlTeh, jmlRoti;
+        double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
+        float diskon = 10 / 100f;
+
+        System.out.print("Masukkan keangotaan (true/false): ");
+        keanggotaan = input.nextBoolean();
+        System.out.print("Masukkan jumlah pembelian kopi: ");
+        jmlKopi = input.nextInt();
+        System.out.print("Masukkan jumlah pembelian teh: ");
+        jmlTeh = input.nextInt();
+        System.out.print("Masukkan jumlah pembelian roti: ");
+        jmlRoti = input.nextInt();
+
+        double totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
+        double nominalBayar = totalHarga - (diskon * totalHarga);
+
+        System.out.println("Keanggotaan pelanggan" + keanggotaan);
+        System.out.println("Item pembelian" + jmlKopi + "kopi," + jmlTeh + "teh, "+ jmlRoti + "roti");
+        System.out.println("Nominal bayar Rp" + nominalBayar);
+    
+    }
+}
